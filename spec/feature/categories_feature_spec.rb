8637ec9aa78cc072c5fc724dca33caca2121e7b3
gedewon <gedewon.hayle.9@gmail.com>
2022-11-16 02:30:24 +0300

    Fix linter locally

--- spec/feature/categories_feature_spec.rb
+++ spec/feature/categories_feature_spec.rb
@@ -1,47 +1,45 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe CategoriesController, :type => :feature do
-    before(:example) do 
-        @user =User.first || User.create(name:'testname',email:'testname@testdomain.com',password:"somerandompassword")
-        @category = Categorie.create(
-            name:"Books",
-            icon:"someIcon",
-            user_id:@user.id,
-            )
-    end
+RSpec.describe CategoriesController, type: :feature do
+  before(:example) do
+    @user = User.first || User.create(name: 'testname', email: 'testname@testdomain.com',
+                                      password: 'somerandompassword')
+    @category = Categorie.create(
+      name: 'Books',
+      icon: 'someIcon',
+      user_id: @user.id
+    )
+  end
 
-    
-    it 'SIGN IN user in / home ' do 
-        visit '/'
-        expect(current_path).to eq(root_path)
-    end
+  it 'SIGN IN user in / home ' do
+    visit '/'
+    expect(current_path).to eq(root_path)
+  end
 
-    it 'LOGIN path with session' do 
-        visit '/'
+  it 'LOGIN path with session' do
+    visit '/'
 
-        click_button "login"
-        
-        fill_in 'Email', with: "testname@testdomain.com"
-        fill_in 'Password', with: "somerandompassword"
-        click_button 'Log in'
+    click_button 'login'
 
-        expect(page).to have_text("Signed in successfully")
-    end 
-    
+    fill_in 'Email', with: 'testname@testdomain.com'
+    fill_in 'Password', with: 'somerandompassword'
+    click_button 'Log in'
 
-    
-    it "Create New Transaction" do 
-        visit '/'
+    expect(page).to have_text('Signed in successfully')
+  end
 
-        click_button "login"
-         
-        fill_in 'Email', with: "testname@testdomain.com"
-        fill_in 'Password', with: "somerandompassword"
-        click_button 'Log in'
+  it 'Create New Transaction' do
+    visit '/'
 
-        click_button "New Categorie" 
-        fill_in 'categorie_name' , with: "testCategory" 
-        click_button 'Save'
-        expect(page).to have_text("Transaction was successfully created.")
-    end 
+    click_button 'login'
+
+    fill_in 'Email', with: 'testname@testdomain.com'
+    fill_in 'Password', with: 'somerandompassword'
+    click_button 'Log in'
+
+    click_button 'New Categorie'
+    fill_in 'categorie_name', with: 'testCategory'
+    click_button 'Save'
+    expect(page).to have_text('Transaction was successfully created.')
+  end
 end
--- spec/models/transferces_model_spec.rb
+++ spec/models/transferces_model_spec.rb
@@ -1,26 +1,26 @@
-require "rails_helper" 
-RSpec.describe Transferce, type: :model do 
-   before(:example){ 
-    @user = User.create(name:'testname',email:'testname@testdomain.com',password:"somerandompassword")
-    @category = Categorie.new(name:"Books",icon:"someIcon", user_id:@user.id)
-   }
-   subject(:transfer) { 
-    Transferce.new(                                                           
-        name: 'C++ programing',                                                           
-        amount: 23,                                                         
-        user_id: @user.id ,                                                        
-        categorie_id: nil,  
+require 'rails_helper'
+RSpec.describe Transferce, type: :model do
+  before(:example) do
+    @user = User.create(name: 'testname', email: 'testname@testdomain.com', password: 'somerandompassword')
+    @category = Categorie.new(name: 'Books', icon: 'someIcon', user_id: @user.id)
+  end
+  subject(:transfer) do
+    Transferce.new(
+      name: 'C++ programing',
+      amount: 23,
+      user_id: @user.id,
+      categorie_id: nil
     )
-   }
-   it 'transferce should be valid with correct attributes' do 
-        expect(subject).to be_valid
-   end 
-   it 'transferce should be invalid with out name' do 
-        subject.name = nil 
-        expect(subject).to_not be_valid
-   end 
-   it 'transferce should be invalid with out amount' do 
-    subject.amount = nil 
+  end
+  it 'transferce should be valid with correct attributes' do
+    expect(subject).to be_valid
+  end
+  it 'transferce should be invalid with out name' do
+    subject.name = nil
     expect(subject).to_not be_valid
-end 
-end 
+  end
+  it 'transferce should be invalid with out amount' do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+end
